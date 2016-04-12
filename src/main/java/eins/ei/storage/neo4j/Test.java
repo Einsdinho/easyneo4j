f38e115eb3953a1efbf20399e1a5e7478ec4e12a
KK.Einsdinho <3337889@qq.com>
2016-04-12 11:54:43 +0800

    down

--- src/main/java/eins/ei/storage/neo4j/Test.java
+++ src/main/java/eins/ei/storage/neo4j/Test.java
@@ -2,6 +2,7 @@ package eins.ei.storage.neo4j;
 
 public class Test {
 	public static void main(String[] args) {
-	DaoRest.sendCypher("create (a:CAR{no:'NB118'})");
+	String str=DaoRest.sendCypher("create (a:CAR{no:'NB888'})");
+	System.out.println(str);
 	}
 }
